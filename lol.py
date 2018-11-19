98c61d4e325776623b985634cb0c5c9cf6794b98
Budjank <42899293+Budjank@users.noreply.github.com>
2018-11-19 15:34:07 +0700

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,11 +1,12 @@
 import os,sys,time
 os.system("clear")
 a=raw_input("Port : ")
+b=raw_input("Subdo : ")
 time.sleep(2)
 print "Tunggu Sebentar..."
 time.sleep(2)
 print "http://localhost:"+a+""
 
 os.system("""
-ssh -R 80:localhost:"""+a+""" serveo.net
+ssh -R """+b+""".serveo.net:80:localhost:"""+a+""" serveo.net
 """)
